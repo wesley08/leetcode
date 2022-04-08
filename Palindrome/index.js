b1494afe76a8415205e9a4844313ecd18d2d219f
wesley08 <43004728+wesley08@users.noreply.github.com>
2022-04-08 17:32:05 +0700

    palindrome

---
+++ Palindrome/index.js
@@ -0,0 +1,36 @@
+var isPalindromeWithTempolary = function (x) {
+  let temp = "";
+  for (let i = x.length - 1; i >= 0; i--) {
+    temp += x[i];
+  }
+  return temp == x;
+};
+const testIsPalindromeWithTempolary = isPalindromeWithTempolary("tetstet");
+console.log(testIsPalindromeWithTempolary);
+
+let isPalindromeWithoutTemp = function (x) {
+  for (let i = 0; i <= x.length / 2; i++) {
+    if (x[i] != x[x.length - i - 1]) return false;
+  }
+  return true;
+};
+const testIsPalindromeWithoutTemp = isPalindromeWithoutTemp("a1sa23aas1a");
+console.log(testIsPalindromeWithoutTemp);
+
+let palindromeRecursive = function (x, i) {
+  if (i < x.length / 2) {
+    if (x[i] != x[x.length - i - 1]) return false;
+    else {
+      return palindromeRecursive(x, i + 1);
+    }
+  } else {
+    return true;
+  }
+};
+
+let isPalindrome = function (x) {
+  return palindromeRecursive(x, 0);
+};
+
+const test = isPalindrome("121");
+console.log(test);
